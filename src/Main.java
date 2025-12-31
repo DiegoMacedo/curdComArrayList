acabbc9c8add7d48a33cbe67a3cc799de2829cca
oliveiramacedao@gmail.com <oliveiramacedao@gmail.com>
2025-12-31 18:49:08 -0400

    Implementação do método cadastrar

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,66 @@ public class Main{
     static List<Produto> produtos = new ArrayList<>();
 
     static void main() {
+        int opcao;
 
+        do {
+            System.out.println("\n===== MENU =====");
+            System.out.println("1 - Cadastrar produto");
+            System.out.println("2 - Listar produtos");
+            System.out.println("3 - Editar produto" );
+            System.out.println("4 - Deletar produto");
+            System.out.println("0 - Sair"           );
+            System.out.print("Escolha: ");
+
+            opcao = teclado.nextInt();
+            teclado.nextLine();//limpa buffer
+
+            switch (opcao){
+                case 1:
+                    cadastrar();
+                    break;
+                case 2:
+                    listar();
+                    break;
+                case 3:
+                    editar();
+                    break;
+                case 4:
+                    deletar();
+                    break;
+                case 0:
+                    sair();
+                    break;
+                default:
+                    System.out.println("Erro, escolha uma opção válida!");
+            }
+
+        }while (opcao != 0);
+    }
+
+    static void cadastrar(){
+        System.out.println("Nome do Produto: ");
+        String nome = teclado.nextLine();
+
+        System.out.println("Preço do Produto: ");
+        double preco = teclado.nextDouble();
+
+        produtos.add(new Produto(nome,preco));
+        System.out.println("Produto Cadastrado com sucesso!");
+    }
+    static void listar(){
+        System.out.println("Listando");
+        for (Produto p: produtos){
+            System.out.println("Nome: " +p.nome + " Preço: " + p.preco );
+        }
+    }
+    static void editar(){
+        System.out.println("Editando");
+    }
+    static void deletar(){
+        System.out.println("Deletando");
+    }
+    static void sair(){
+        System.out.println("Saindo");
     }
 }
